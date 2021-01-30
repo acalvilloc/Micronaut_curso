d5e37af2442e711aba727f1bbe6fb4c32809ae4e
Alexis Calvillo <acalvillo@asgbpm.com>
2021-01-30 10:10:59 -0600

    JWT Auth

---
+++ JWT/complete/src/test/groovy/example/micronaut/DeclarativeHttpClientWithJwtSpec.groovy
@@ -0,0 +1,34 @@
+package example.micronaut
+
+import com.nimbusds.jwt.JWTParser
+import com.nimbusds.jwt.SignedJWT
+import io.micronaut.security.authentication.UsernamePasswordCredentials
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Specification
+
+import javax.inject.Inject
+
+@MicronautTest
+class DeclarativeHttpClientWithJwtSpec extends Specification {
+
+    @Inject
+    AppClient appClient 
+
+    def "Verify JWT authentication works with declarative @Client"() {
+        when: 'Login endpoint is called with valid credentials'
+        UsernamePasswordCredentials creds = new UsernamePasswordCredentials("sherlock", "password")
+        BearerAccessRefreshToken loginRsp = appClient.login(creds) 
+
+        then:
+        loginRsp
+        loginRsp.accessToken
+        JWTParser.parse(loginRsp.accessToken) instanceof SignedJWT
+
+        when:
+        String msg = appClient.home("Bearer ${loginRsp.accessToken}") 
+
+        then:
+        msg == 'sherlock'
+    }
+}
